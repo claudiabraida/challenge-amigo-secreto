9303f2fa810bf433fed0f248e34c3526239829ea
claudiabraida <braidacreaciones@gamail.com>
2025-02-15 22:53:34 -0300

    feat: capture input value for addFriend feat: validate non-empty input for addFriend feat: update friends array in addFriend > feat: clear input field after addFriend

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let arrayAmigos = []
+
+/* 💥💥💥💥💥💥 VARIABLES 💥💥💥💥💥💥 */
+const $ = element => document.querySelector(element);
+$inputAmigo = $("#amigo")
+
+
+/* 💥💥💥💥💥💥 FUNCION LIMPIAR CAMPO💥💥💥💥💥💥 */
+function limpiarCampo () {
+  $inputAmigo.value = ""
+}
+
+
+/* 💥💥💥💥💥💥 FUNCION MANEJO DOM 💥💥💥💥💥💥 */
+function agregarAmigo () {
+  const nombreIngresado = $inputAmigo.value.trim().replace(/\s+/g, " ")
+  limpiarCampo()
+  
+  if(nombreIngresado === "") {
+    alert("Por favor, ingrese un nombre.")
+  } else {
+    actualizarArray (nombreIngresado)
+  }  
+
+}
+
+/* 💥💥💥💥💥💥 FUNCION ACTUALIZAR DATOS (ARRAY) 💥💥💥💥💥💥 */
+function actualizarArray (nombreIngresado) {
+  arrayAmigos.push(nombreIngresado)
+  console.log(arrayAmigos)
+  
+}
